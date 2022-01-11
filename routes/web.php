ee9951f0cceaf24e4e1f850defe1b41f5d2acdf1
Agus Salim <aguss6460@gmail.com>
2022-01-11 14:22:01 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Route::get("/", [PesananController::class,"index"])->name("pesanan.index");
 });
 
 Route::prefix("pesanan")->group(function(){
